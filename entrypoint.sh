ef43bd4458986f489e9f88f5f9e095fa548a3456
brconnell4 <branden.connell@arizona.aaa.com>
2023-07-14 07:41:28 -0700

    fixing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ helm repo add ${REPO_NAME} ${HELM_REPO_URL}
 helm repo add stable https://charts.helm.sh/stable
 
 if [ "${UPDATE_DEPENDENCIES}" = "false" ]; then
-  helm package --version="${HELM_TAG}" --app-version="${HELM_TAG}" ${CHART_SUBDIR}/.
+  helm package --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
 else
-  helm package -u --version="${HELM_TAG}" --app-version="${HELM_TAG}" ${CHART_SUBDIR}/.
+  helm package -u --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
 fi
 
-RESPONSE=$(curl --data-binary "@${CHART_SUBDIR}-$(cat .tags).tgz" ${HELM_REPO_URL}/api/charts)
+RESPONSE=$(curl --data-binary "@${CHART_SUBDIR}-${HELM_TAG}.tgz" "${HELM_REPO_URL}"/api/charts)
 echo "$RESPONSE"
 
 if [ "$RESPONSE" = '{"saved":true}' ]; then
