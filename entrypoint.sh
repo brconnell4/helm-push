6eb16a18e1d0ec95ba5714b2e8dff84499b0f86f
brconnell4 <branden.connell@arizona.aaa.com>
2023-07-14 08:36:32 -0700

    debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 helm repo add ${INPUT_REPO_NAME} ${INPUT_HELM_REPO_URL}
 helm repo add stable https://charts.helm.sh/stable
 
-if [ "${UPDATE_DEPENDENCIES}" = "false" ]; then
+if [ "${INPUT_UPDATE_DEPENDENCIES}" = "false" ]; then
   helm package --version="${INPUT_HELM_TAG}" --app-version="${INPUT_HELM_TAG}" "${INPUT_CHART_SUBDIR}"/.
 else
   helm package -u --version="${INPUT_INPUT_HELM_TAG}" --app-version="${INPUT_HELM_TAG}" "${INPUT_CHART_SUBDIR}"/.
