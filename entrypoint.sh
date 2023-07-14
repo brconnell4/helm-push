532f2bfdb816af4186f89f45c10f9e1833f197d4
brconnell4 <branden.connell@arizona.aaa.com>
2023-07-14 08:07:19 -0700

    troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,23 @@
 #!/bin/sh -l
 
-helm repo add ${REPO_NAME} ${HELM_REPO_URL}
-helm repo add stable https://charts.helm.sh/stable
+echo ${REPO_NAME} ${HELM_REPO_URL}
+ls -la
+pwd
 
-if [ "${UPDATE_DEPENDENCIES}" = "false" ]; then
-  helm package --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
-else
-  helm package -u --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
-fi
-
-RESPONSE=$(curl --data-binary "@${CHART_SUBDIR}-${HELM_TAG}.tgz" "${HELM_REPO_URL}"/api/charts)
-echo "$RESPONSE"
-
-if [ "$RESPONSE" = '{"saved":true}' ]; then
-  exit 0
-else
-  exit 1
-fi
+#helm repo add ${REPO_NAME} ${HELM_REPO_URL}
+#helm repo add stable https://charts.helm.sh/stable
+#
+#if [ "${UPDATE_DEPENDENCIES}" = "false" ]; then
+#  helm package --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
+#else
+#  helm package -u --version="${HELM_TAG}" --app-version="${HELM_TAG}" "${CHART_SUBDIR}"/.
+#fi
+#
+#RESPONSE=$(curl --data-binary "@${CHART_SUBDIR}-${HELM_TAG}.tgz" "${HELM_REPO_URL}"/api/charts)
+#echo "$RESPONSE"
+#
+#if [ "$RESPONSE" = '{"saved":true}' ]; then
+#  exit 0
+#else
+#  exit 1
+#fi
