e063093f8f5b0798d4f017aa405247a25d8b3a73
brconnell4 <branden.connell@arizona.aaa.com>
2023-07-24 07:19:03 -0700

    adding calculate version

--- calculate-version/entrypoint.sh
+++ calculate-version/entrypoint.sh
@@ -8,9 +8,12 @@ set -v
 
 # Determine the version using GitVersion
 GITVERSION_JSON=$(gitversion /output json /showvariable NuGetVersionV2)
+echo "$GITVERSION_JSON"
 
 # Extract branch name from Github environment variable
 BRANCH_NAME=$(echo "$GITHUB_HEAD_REF" | sed 's/features\///')
+echo "$BRANCH_NAME"
+
 
 # Determine if prod flag is true
 if [ "$1" == "true" ]; then
